114dd9bda5947715c4257fba2420aad4ca732eeb
Brandon Bowen <brandonbowenbiz@gmail.com>
2025-10-10 21:09:41 -0700

    Added dropdown menu to navbar, sidebar, and dashboard cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,8 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
-
-        {children}
+        <div className='flex'>
+          <div className='hidden md:block h-[100vh] w-[300px]'>
+            <Sidebar />
+          </div>
+          <div className='p-5 w-full md:max-w-[1140px]'>{children}</div>
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,37 @@
-import { Button } from '@./../../components/ui/button';
+import DashboardCard from '@/components/dashboard/DashboardCard';
+import { Folder, MessageCircle, Newspaper, User } from 'lucide-react';
 
 export default function Home() {
   return (
     <>
-      <h1 className='text-2xl'>Dashboard</h1>
-      <Button variant='destructive' size='lg' className='text-blue-400'>
-        Click me
-      </Button>
+      <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
+        <DashboardCard
+          title='Posts'
+          count={100}
+          icon={<Newspaper className='text-slate-500' size={72} />}
+        />
+      </div>
+      <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
+        <DashboardCard
+          title='Categories'
+          count={12}
+          icon={<Folder className='text-slate-500' size={72} />}
+        />
+      </div>
+      <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
+        <DashboardCard
+          title='Users'
+          count={750}
+          icon={<User className='text-slate-500' size={72} />}
+        />
+      </div>
+      <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
+        <DashboardCard
+          title='Comments'
+          count={1200}
+          icon={<MessageCircle className='text-slate-500' size={72} />}
+        />
+      </div>
     </>
   );
 }

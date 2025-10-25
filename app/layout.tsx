d1ff6b80884945c7700ad745b64a5bf76b44b5f7
Brandon Bowen <brandonbowenbiz@gmail.com>
2025-10-24 21:09:03 -0700

    Added theme toggler and auth page with login and register forms

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/Navbar';
-import Sidebar from '@/components/Sidebar';
 import { Toaster } from '@/components/ui/sonner';
+import { ThemeProvider } from '@/components/providers/ThemeProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,14 +28,14 @@ export default function RootLayout({
     <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Navbar />
-        <div className='flex'>
-          <div className='hidden md:block h-[100vh] w-[300px]'>
-            <Sidebar />
-          </div>
-          <div className='p-5 w-full md:max-w-[1140px]'>{children}</div>
-        </div>
-        <Toaster position='top-right' richColors closeButton />
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='light'
+          enableSystem={false}
+          storageKey='dashboard-theme'>
+          {children}
+          <Toaster position='top-right' richColors closeButton />
+        </ThemeProvider>
       </body>
     </html>
   );
